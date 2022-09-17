dd2429210dfd7fd0df833649d5a1dd1fa4567044
Liam <86295301+8bitAndy@users.noreply.github.com>
2022-09-17 19:09:08 +0800

    Added scene transition within GUI
    Implemented working main menu and scene transitions

--- Monte Carlo Methods/curve1.lua
+++ Monte Carlo Methods/curve1.lua
@@ -32,9 +32,10 @@ function generatePoints()
         local areaOfRectangle = graphSize * graphSize;
     
         -- The amount of points that get placed under the function curve
-        
-
         local pointsUnderCurve = 0;
+
+        -- The result of calculations to be put into the scene group within the create function
+        local results = {}
         
         
     
@@ -64,11 +65,21 @@ function generatePoints()
     
         print("Area under the curve is: " .. areaUnderCurve);
         print("Relative error is: " .. relativeError);
-    
-        -- Simple test for displaying results text onto device screen
-        local sceneText = display.newText("y = x", display.contentCenterX, display.contentCenterY - 200, "Arial", "40");
-        local resultText = display.newText("The area under the curve is " .. areaUnderCurve, display.contentCenterX, display.contentCenterY, "Arial", "16");
-        local pointsUnderCurveText = display.newText("There are " .. pointsUnderCurve .. " points under the curve", display.contentCenterX, display.contentCenterY + 25, "Arial", "16");
+
+        print("adding to table")
+        -- Add area of curve into results table
+        table.insert(results, 0, areaUnderCurve);
+
+        -- Add points under curve into result table
+        table.insert(results, 1, pointsUnderCurve);
+
+        -- Add margin of error into table
+        table.insert(results, 2, relativeError);
+
+        print("added to table")
+
+        -- Send to create function
+        return results;
 end
 
 function generatePoints2()
@@ -220,7 +231,7 @@ function scene:create( event )
     -- Code here runs when the scene is first created but has not yet appeared on screen
     print("in new scene \n")
 
-    generatePoints();
+    --generatePoints();
     print("---------------------------------------\n")
     generatePoints2();
     print("---------------------------------------\n")
@@ -228,40 +239,94 @@ function scene:create( event )
     print("---------------------------------------\n")
     generatePoints4();
 
-    -- Testing scene change with a timer
-    --timer.performWithDelay( 50000 )
-    --composer.removeScene("curve1")
-    --composer.gotoScene("mainMenu")
+    -- Creating line for graph boundaries
     local yAxisLine = display.newLine( display.contentCenterX - 150, display.contentCenterY + 100 , display.contentCenterX - 150, display.contentCenterY - 200 )
     local xAxisLine = display.newLine( display.contentCenterX - 150, display.contentCenterY + 100 , display.contentCenterX + 150, display.contentCenterY + 100 )
-    --star:append( 305,165, 243,216, 265,290, 200,245, 135,290, 157,215, 95,165, 173,165, 200,90 )
+
+    -- Creating test line of function y = x
+    local functionLine = display.newLine( display.contentCenterX - 150, display.contentCenterY + 100 , display.contentCenterX + 150, display.contentCenterY - 200)
+    -- Change the colour of the line
+    functionLine:setStrokeColor( 1, 0, 0, 1)
+    -- Change the width of the axis line
+    functionLine.strokeWidth = 3;
+    -- Add function line into scene group for composer scene handling
+    sceneGroup:insert(functionLine);
 
     -- Create line by appending points to the current line for the graph
-
+    --star:append( 305,165, 243,216, 265,290, 200,245, 135,290, 157,215, 95,165, 173,165, 200,90 )
 
     -- Change the colour of the line
-    yAxisLine:setStrokeColor( 1, 0, 1, 1)
+    yAxisLine:setStrokeColor( 1, 1, 1, 1)
     -- Change the width of the axis line
     yAxisLine.strokeWidth = 4
 
     -- Change the colour of the line
-    xAxisLine:setStrokeColor( 0, 0, 1, 1)
+    xAxisLine:setStrokeColor( 1, 1, 1, 1)
     -- Change the width of the axis line
     xAxisLine.strokeWidth = 4
 
-    -- Test button
-    local optionsButton = display.newRoundedRect( display.contentCenterX, display.contentCenterY + 200, 150, 50, 12 )
+    -- Options button
+    local optionsButton = display.newRoundedRect( display.contentCenterX, display.contentCenterY + 250, 150, 50, 12 )
     local optionsButtonText = display.newText("Options", optionsButton.x, optionsButton.y, "Arial", "20");
     optionsButton.strokeWidth = 3
     optionsButton:setFillColor( 0, 0.5, 1 )
     optionsButton:setStrokeColor( 1, 1, 1 )
+    -- Add button and button text into scene group
+    sceneGroup:insert(optionsButton)
+    sceneGroup:insert(optionsButtonText)
+
+    -- Options button
+    local backButton = display.newRoundedRect( display.contentCenterX - 110, display.contentCenterY + 250, 50, 50, 10 )
+    local backButtonText = display.newText("<", backButton.x, backButton.y, "Arial", "20");
+    backButton.strokeWidth = 3
+    backButton:setFillColor( 0, 0.5, 1 )
+    backButton:setStrokeColor( 1, 1, 1 )
+    -- Add button and button text into scene group
+    sceneGroup:insert(backButton)
+    sceneGroup:insert(backButtonText)
+
+    -- Options button
+    local forwardButton = display.newRoundedRect( display.contentCenterX + 110, display.contentCenterY + 250, 50, 50, 10 )
+    local forwardButtonText = display.newText(">", forwardButton.x, forwardButton.y, "Arial", "20");
+    forwardButton.strokeWidth = 3
+    forwardButton:setFillColor( 0, 0.5, 1 )
+    forwardButton:setStrokeColor( 1, 1, 1 )
+    -- Add button and button text into scene group
+    sceneGroup:insert(forwardButton)
+    sceneGroup:insert(forwardButtonText)
+
+    -- title of graph
+    local sceneText = display.newText("Curve for y = x", display.contentCenterX, display.contentCenterY - 250, "Arial", "30");
+    sceneText:setTextColor(1, 0, 1);
+
+    -- Get area under curve and amount of points and put into a table
+    local returnedResultsTable = generatePoints();
+
+    -- Assign values from table into variables for use later
+    local areaUnderCurve = returnedResultsTable[0];
+    local pointsUnderCurve = returnedResultsTable[1];
+    local marginOfError = returnedResultsTable[2];
+
+    -- Simple test for displaying results text onto device screen
+    local resultText = display.newText("The area under the curve is " .. areaUnderCurve, display.contentCenterX, display.contentCenterY + 150, "Arial", "16");
+    local pointsUnderCurveText = display.newText("There are " .. pointsUnderCurve .. " points under the curve", display.contentCenterX, display.contentCenterY + 175, "Arial", "16");
+    local marginOfErrorText = display.newText("The margin of error is " .. marginOfError, display.contentCenterX, display.contentCenterY + 200, "Arial", "16");
+
+    -- Put results and points under curve into scnee group
+    sceneGroup:insert(resultText);
+    sceneGroup:insert(pointsUnderCurveText);
+    sceneGroup:insert(marginOfErrorText);
 
     -- Add event handler to button
-    optionsButton:addEventListener(optionsButtonPress);
+    optionsButton:addEventListener("tap", optionsButtonPress);
 
+    sceneGroup:insert(sceneText)
     -- Add objects into scene group
     sceneGroup:insert(optionsButton)
     sceneGroup:insert(optionsButtonText)
+    sceneGroup:insert(yAxisLine)
+    sceneGroup:insert(xAxisLine)
+
     
 end
 
