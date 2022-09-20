e074bedd4f1a2b5c59d463d5385eead00d86176d
Liam <86295301+8bitAndy@users.noreply.github.com>
2022-09-20 17:02:04 +0800

    Fixed curve 3 and 4 algorithm
    Update algorithms for area detection within curve 3 and curve 4 functions. Works correctly and produces output within margin of error

--- Monte Carlo Methods/main.lua
+++ Monte Carlo Methods/main.lua
@@ -15,33 +15,46 @@
 --composer.gotoScene("mainMenu")
 
 
-
 -- Trying to get curve 3 working
 function generatePoints3()
         
 
     -- Input variables, fixed number of points is 10,000
     local totalPoints = 10000;
-    -- Variable to hold the x and y length and height of the graph
-    local graphDomain = 7;
+
+    -- Calculate the domain of the graph by using the x upper and lower limit
+    local minimumX = -2;
+    local maximumX = 5;
+    local graphDomain = math.abs(minimumX  - maximumX);
+    -- The highest and lowest y value used for limits
+    local minY = 0;
+    local maxY = 0;
+    
+    -- This detects the maximum value that y will take within the x limits of the curve
+    for currentX = minimumX, maximumX, 0.001 do
+        local currentY = (-(currentX^3) + (6 * (currentX ^ 2)) - currentX + 17)
+        if currentY > maxY then
+            maxY = currentY 
+        end
+    end
+
+    print("This is the max y picked: " .. maxY)
 
     -- The amount of points that get placed under the function curve
     local pointsUnderCurve = 0;
-
-    local minY = 0;
-    local maxY = 51;
 
     -- Area of Cartesian plane
     local areaOfRectangle = graphDomain * maxY;
     
     -- If below the line then add one to points below the line
-    for i=1, 10000, 1 do
+    for i=1, totalPoints, 1 do
 
         -- Generate a random floating point number
-        local x = math.abs(math.random(-2, 4) + math.random());
+        local x = math.abs(math.random(minimumX, maximumX - 1));
 
         -- Generate a random floating point number
-        local y = math.abs(math.random(minY, maxY - 1) + math.random());
+
+        local y = minimumX + (maxY * math.random())
 
         -- A point is under the curve if it meets this condition
         if  y <= (-(x^3) + (6 * (x ^ 2)) - x + 17) then
@@ -52,7 +65,6 @@ function generatePoints3()
     
 
     print("\nThere are " .. pointsUnderCurve .. " points under the cubic");
-    pointsUnderCurve = pointsUnderCurve
     -- Calculate area under the curve
     local temp = (pointsUnderCurve / totalPoints);
     local areaUnderCurve =  temp * areaOfRectangle
@@ -64,4 +76,75 @@ function generatePoints3()
     print("Relative error is: " .. relativeError);
 end
 
+
+
+
+
+-- Trying to get curve 4 working
+function generatePoints4()
+        
+
+    -- Input variables, fixed number of points is 10,000
+    local totalPoints = 10000;
+
+    -- Calculate the domain of the graph by using the x upper and lower limit
+    local minimumX = 0;
+    local maximumX = 4;
+    local graphDomain = math.abs(minimumX - maximumX);
+    -- The highest and lowest y value used for limits
+    local minY = 0;
+    local maxY = 0;
+
+    -- Eulers number, used for creating the curve below
+    local e = 2.71828;
+    
+    -- This detects the maximum value that y will take within the x limits of the curve
+    for currentX = minimumX, maximumX, 1 do
+        local currentY = ((15 * currentX ^ 3) + (21 * currentX ^ 2) + (41 * currentX) + 3) ^ (1/4)  * (e ^ (-0.5 * currentX))
+        if currentY > maxY then
+            maxY = currentY 
+        end
+    end
+
+    print("This is the max y picked: " .. maxY)
+
+    local range = math.abs(maxY - minY)
+    print("range is: " .. range)
+
+    -- The amount of points that get placed under the function curve
+    local pointsUnderCurve = 0;
+
+    -- Area of Cartesian plane
+    local areaOfRectangle = graphDomain * maxY;
+    
+    -- If below the line then add one to points below the line
+    for i=1, totalPoints, 1 do
+
+        -- Generate a random floating point number
+        local x = math.abs(math.random(minimumX, maximumX - 1) + math.random());
+
+        -- Generate a random floating point number
+        local y = minimumX + (range * math.random())
+
+        -- A point is under the curve if it meets this condition
+        if  y < ((15 * x ^ 3) + (21 * x ^ 2) + (41 * x) + 3) ^ (1/4)  * (e ^ (-0.5 * x)) then
+            pointsUnderCurve = pointsUnderCurve + 1;
+        end
+    end
+
+    
+    print("\nThere are " .. pointsUnderCurve .. " points under the cubic");
+    -- Calculate area under the curve
+    local temp = (pointsUnderCurve / totalPoints);
+    local areaUnderCurve =  temp * areaOfRectangle
+
+    -- Relative error of actual result
+    local relativeError = math.abs((areaUnderCurve - 5.7674) / 5.7674)
+
+    print("Area under the curve is: " .. areaUnderCurve);
+    print("Relative error is: " .. relativeError);
+end
+
+
 generatePoints3()
+generatePoints4()
